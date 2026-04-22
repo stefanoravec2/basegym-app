85e4dcbe295563b3c59b053ef61fb384d1b66d8d
stefanoravec2 <stefanoravec2@gmail.com>
2026-04-22 15:19:55 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Starý projekt kde sú tréningy a rezervácie
 const supabaseUrl = 'https://njkxzbwzmhovuzuoyndj.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qa3h6Ynd6bWhvdnV6dW95bmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NTgxOTgsImV4cCI6MjA5MTUzNDE5OH0.FmZldAabAE5Ac9YebKfiwwtzlC6ScRL77wJhTgZ6hjk'
 
